02d0725204ddce9e2bfb9b2fc1bac15818268707
mariarodriguescosta18-cloud <maria.rodrigues.costa18@escola.pr.gov.br>
2026-07-10 20:54:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarNome() {
+   let nome = Document.getElementById("mensagem").innerText =
+      
+   document.getElementById("mensagem").innerText =
+        "Bem-vindo, " +     nome + "!";
+}
